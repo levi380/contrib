27e7113404bbb69c61b315608f05a11e628bad52
zhangsan <longfeifengwu66@gmail.com>
2025-05-20 10:29:08 +0700

    +

--- helper/consts_game.go
+++ helper/consts_game.go
@@ -6,7 +6,7 @@ const (
 	GameTypeSlot    = "3" //老虎机
 	GameTypeSport   = "4" //体育
 	GameTypeTable   = "5" //棋牌
-	GameTypeESport  = "6" //电竞
+	GameTypeMini    = "6" //小游戏
 	GameTypeLottery = "7" //彩票
 )
 
@@ -17,7 +17,7 @@ const (
 	PP       = "102" //PP视讯
 	Habanero = "103" //Habanero视讯
 	Sexy     = "104" //Sexy视讯
-
+	WE       = "105" //WE视讯
 	// 捕鱼
 	JILIFish      = "201" //JILI捕鱼
 	JDBFish       = "202" //JDB捕鱼
@@ -26,6 +26,7 @@ const (
 	YGRFish       = "205" //YGR捕鱼
 	YellowbatFish = "206" //Yellowbat捕鱼
 	TPGFish       = "207" //TPG捕鱼
+	WEFish        = "208" //WE捕鱼
 	// 老虎机
 	PG            = "301" //PG老虎机
 	PPSlot        = "302" //pp老虎机
@@ -38,17 +39,19 @@ const (
 	NetEntSlot    = "309" //NetEnt老虎机
 	NLCSlot       = "310" //NLC老虎机
 	RedTigerSlot  = "311" //RedTiger老虎机
-	BngSlot       = "312" //BngSlot老虎机
-	FunkySlot     = "313" //FunkySlot老虎机
-	HabaneroSlot  = "314" //HabaneroSlot老虎机
-	RTGSlot       = "315" //RTGSlot老虎机
-	TPGSlot       = "316" //TPGSlot老虎机
-	VIASlot       = "317" //VIASlot老虎机
-	YellowbatSlot = "318" //YellowbatSlot老虎机
-	YGRSlot       = "319" //YGRSlot
+	BngSlot       = "312" //Bng老虎机
+	FunkySlot     = "313" //Funky老虎机
+	HabaneroSlot  = "314" //Habanero老虎机
+	RTGSlot       = "315" //RTG老虎机
+	TPGSlot       = "316" //TPG老虎机
+	VIASlot       = "317" //VIA老虎机
+	YellowbatSlot = "318" //Yellowbat老虎机
+	YGRSlot       = "319" //YGR老虎机
+	WESlot        = "320" //WE老虎机
 	//体育
-	FBSport = "401" //FBSport体育
-	APSport = "402" //APSport体育
+	FBSport = "401" //FB体育
+	APSport = "402" //AP体育
+	WESport = "403" //WE体育
 	//棋牌
 	JILIPoker      = "501" //JILI棋牌
 	JDBPoker       = "502" //JDB棋牌
@@ -56,6 +59,9 @@ const (
 	PGPoker        = "504" //PG棋牌
 	TPGPoker       = "505" //TPG棋牌
 	HabaneroPoker  = "506" //Habanero棋牌
+	WEPoker        = "507" //WE棋牌
+	//小游戏
+	WEMini = "601" //WE小游戏
 	//彩票
 	JILIBingo      = "701" //JILI Bingo
 	YellowbatBingo = "702" //Yellowbat Bingo

d4f51297e954e7adce0548ad4edb70e74dfdc409
Levi <levi@gmail.com>
2025-05-28 14:32:38 +0700

    xg

--- helper/rdtsc_decl.go
+++ helper/rdtsc_decl.go
@@ -2,8 +2,9 @@ package helper
 
 import (
 	"crypto/tls"
-	"github.com/valyala/fasthttp"
 	"time"
+
+	"github.com/valyala/fasthttp"
 )
 
 var (
@@ -39,6 +40,36 @@ func HttpDoTimeout(requestBody []byte, method string, requestURI string, headers
 
 	// time.Second * 30
 	err := fc.DoTimeout(req, resp, timeout)
+
+	return resp.Body(), resp.StatusCode(), err
+}
+
+func HttpProxyTimeout(hc *fasthttp.Client, requestBody []byte, method string, requestURI string, headers map[string]string, timeout time.Duration) ([]byte, int, error) {
+
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+
+	defer func() {
+		fasthttp.ReleaseResponse(resp)
+		fasthttp.ReleaseRequest(req)
+	}()
+
+	req.SetRequestURI(requestURI)
+	req.Header.SetMethod(method)
+
+	switch method {
+	case "POST", "PUT", "DELETE":
+		req.SetBody(requestBody)
+	}
+
+	if headers != nil {
+		for k, v := range headers {
+			req.Header.Set(k, v)
+		}
+	}
+
+	// time.Second * 30
+	err := hc.DoTimeout(req, resp, timeout)
 
 	return resp.Body(), resp.StatusCode(), err
 }

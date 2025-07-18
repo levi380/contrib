b285e809908f4719e0e759d97edd58715f504d1f
Levi <levi@gmail.com>
2025-07-18 23:35:24 +0700

    xg

--- conn/connection.go
+++ conn/connection.go
@@ -6,16 +6,13 @@ import (
 	"database/sql"
 	"encoding/base64"
 	"fmt"
-	"github.com/jmoiron/sqlx"
 	"log"
 	"time"
 
+	"github.com/jmoiron/sqlx"
+
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/qiniu/qmgo"
-	qnOpts "github.com/qiniu/qmgo/options"
 	"github.com/redis/go-redis/v9"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"golang.org/x/crypto/chacha20poly1305"
 )
 
@@ -45,69 +42,6 @@ func Chacha20Decode(ciphertext, pass string) ([]byte, error) {
 	nonce := make([]byte, chacha20poly1305.NonceSizeX)
 
 	return aead.Open(nil, nonce, decode, nil)
-}
-
-func InitMongo(ctx context.Context, url, username, passwd, dbname string) (*qmgo.Client, *qmgo.Database) {
-
-	var (
-		timeout     int64  = 2000
-		maxPoolSize uint64 = 100
-		minPoolSize uint64 = 0
-		durl               = []byte(url)
-		dusername          = []byte(username)
-		dpasswd            = []byte(passwd)
-		err         error
-	)
-	if xxteaKey != "" {
-		durl, err = Chacha20Decode(url, xxteaKey)
-		if err != nil {
-			fmt.Println("InitMongo", url, err)
-			log.Fatalln(err)
-		}
-
-		dusername, err = Chacha20Decode(username, xxteaKey)
-		if err != nil {
-			fmt.Println("InitMongo", username, err)
-			log.Fatalln(err)
-		}
-
-		dpasswd, err = Chacha20Decode(passwd, xxteaKey)
-		if err != nil {
-			fmt.Println("InitMongo", passwd, err)
-			log.Fatalln(err)
-		}
-	}
-
-	clientOptions := &options.ClientOptions{}
-	// 设置认证信息
-	credential := options.Credential{
-		Username:   string(dusername),
-		Password:   string(dpasswd),
-		AuthSource: dbname,
-	}
-	clientOptions.SetAuth(credential)
-	opts := qnOpts.ClientOptions{
-		ClientOptions: clientOptions,
-	}
-
-	cfg := qmgo.Config{
-		Uri:              string(durl),
-		ConnectTimeoutMS: &timeout,
-		MaxPoolSize:      &maxPoolSize,
-		MinPoolSize:      &minPoolSize,
-		ReadPreference:   &qmgo.ReadPref{Mode: readpref.SecondaryMode, MaxStalenessMS: 100 * 1000},
-	}
-
-	cli, err := qmgo.NewClient(ctx, &cfg, opts)
-	if err != nil {
-		log.Fatalf("initMongoClient failed: %s", err.Error())
-	}
-	err = cli.Ping(5)
-	if err != nil {
-		log.Fatalf("MongoClient ping failed: %s", err.Error())
-	}
-
-	return cli, cli.Database(dbname)
 }
 
 func InitDB(dsn string, maxIdleConn, maxOpenConn int) *sql.DB {
@@ -155,66 +89,13 @@ func InitDBX(dsn string, maxIdleConn, maxOpenConn int) *sqlx.DB {
 	return db
 }
 
-func InitRedisSentinelCluster(dsn []string, psd, name string, db, poolSize int) *redis.ClusterClient {
+func InitRedisSentinel(dsn []string, username, psd, name string, db, poolSize int) *redis.Client {
 
-	var (
-		dst = []byte(psd)
-		err error
-	)
-
-	if xxteaKey != "" {
-		dst, err = Chacha20Decode(psd, xxteaKey)
-		if err != nil {
-			fmt.Println("InitRedisSentinel", psd, err)
-			log.Fatalln(err)
-		}
-	}
-
-	reddb := redis.NewFailoverClusterClient(&redis.FailoverOptions{
-		MasterName:            name,
-		SentinelAddrs:         dsn,
-		Password:              string(dst), // no password set
-		DB:                    db,          // use default DB
-		Protocol:              2,
-		ReplicaOnly:           true,
-		DialTimeout:           10 * time.Second,
-		ReadTimeout:           30 * time.Second,
-		WriteTimeout:          30 * time.Second,
-		PoolSize:              poolSize,
-		PoolTimeout:           30 * time.Second,
-		MaxRetries:            2,
-		MaxIdleConns:          20,
-		ConnMaxLifetime:       5 * time.Minute,
-		ContextTimeoutEnabled: false,
-	})
-	pong, err := reddb.Ping(ctx).Result()
-	if err != nil {
-		log.Fatalf("InitRedisSentinel failed: %s", err.Error())
-	}
-	fmt.Println(pong, err)
-
-	return reddb
-}
-
-func InitRedisSentinel(dsn []string, psd, name string, db, poolSize int) *redis.Client {
-
-	var (
-		dst = []byte(psd)
-		err error
-	)
-	if xxteaKey != "" {
-		dst, err = Chacha20Decode(psd, xxteaKey)
-		if err != nil {
-			fmt.Println("InitRedisSentinel", psd, err)
-			log.Fatalln(err)
-		}
-	}
-
-	reddb := redis.NewFailoverClient(&redis.FailoverOptions{
+	cfg := &redis.FailoverOptions{
 		MasterName:    name,
 		SentinelAddrs: dsn,
-		Password:      string(dst), // no password set
-		DB:            db,          // use default DB
+		Password:      psd, // no password set
+		DB:            db,  // use default DB
 		DialTimeout:   10 * time.Second,
 		ReadTimeout:   30 * time.Second,
 		WriteTimeout:  30 * time.Second,
@@ -225,8 +106,13 @@ func InitRedisSentinel(dsn []string, psd, name string, db, poolSize int) *redis.
 		//MaxActiveConns:        20,
 		ConnMaxLifetime:       5 * time.Minute,
 		ContextTimeoutEnabled: false,
-		UnstableResp3: true,
-	})
+		UnstableResp3:         true,
+	}
+
+	if username != "" {
+		cfg.Username = username
+	}
+	reddb := redis.NewFailoverClient(cfg)
 	pong, err := reddb.Ping(ctx).Result()
 	if err != nil {
 		log.Fatalf("InitRedisSentinel failed: %s", err.Error())
@@ -236,12 +122,11 @@ func InitRedisSentinel(dsn []string, psd, name string, db, poolSize int) *redis.
 	return reddb
 }
 
-func InitRedisCluster(dsn []string, passwd string, poolSize int) *redis.ClusterClient {
+func InitRedis(dsn string, passwd string, poolSize int) *redis.Client {
 
-	reddb := redis.NewClusterClient(&redis.ClusterOptions{
-
-		Addrs:        dsn,
-		Password:     passwd, // no password set
+	reddb := redis.NewClient(&redis.Options{
+		Addr: dsn,
+		//Username: "user",
 		DialTimeout:  10 * time.Second,
 		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 30 * time.Second,
@@ -252,6 +137,7 @@ func InitRedisCluster(dsn []string, passwd string, poolSize int) *redis.ClusterC
 		//MaxActiveConns:        20,
 		ConnMaxLifetime:       5 * time.Minute,
 		ContextTimeoutEnabled: false,
+		Password:              passwd,
 	})
 	pong, err := reddb.Ping(ctx).Result()
 	if err != nil {

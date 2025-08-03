0e9031de8fb298f3ab3f4b32a8e493da5da3a130
Levi <levi@Levi.local>
2025-08-03 11:45:24 +0800

    xg

--- beanstalkpool/pool.go
+++ beanstalkpool/pool.go
@@ -9,7 +9,10 @@ import (
 	"github.com/jackc/puddle/v2"
 )
 
-var pool *puddle.Pool[*beanstalk.Conn]
+var (
+	pool *puddle.Pool[*beanstalk.Conn]
+	ctx  = context.Background()
+)
 
 func New(addr string, maxIdle, maxActive int, dialTimeout time.Duration) {
 
@@ -29,10 +32,73 @@ func New(addr string, maxIdle, maxActive int, dialTimeout time.Duration) {
 	}
 }
 
+func Put(topic string, body []byte, pri uint32, delay, ttr time.Duration) (id uint64, err error) {
+
+	res, err := pool.Acquire(ctx)
+	if err != nil {
+		return 0, err
+	}
+	defer res.Release()
+
+	tube := beanstalk.NewTube(res.Value(), topic)
+
+	id, err1 := tube.Put(body, pri, delay, ttr)
+	if err1 != nil {
+		return 0, err1
+	}
+	return id, nil
+}
+
+func Reserve(topic []string, timeout time.Duration) (id uint64, body []byte, err error) {
+
+	res, err := pool.Acquire(ctx)
+	if err != nil {
+		return 0, nil, err
+	}
+	defer res.Release()
+
+	tube := beanstalk.NewTubeSet(res.Value(), topic...)
+	return tube.Reserve(timeout)
+}
+
+func Delete(id uint64) error {
+
+	res, err := pool.Acquire(ctx)
+	if err != nil {
+		return err
+	}
+	defer res.Release()
+
+	return res.Value().Delete(id)
+}
+
+func Release(id uint64, pri uint32, delay time.Duration) error {
+
+	res, err := pool.Acquire(ctx)
+	if err != nil {
+		return err
+	}
+	defer res.Release()
+
+	return res.Value().Release(id, pri, delay)
+}
+
+func Bury(id uint64, pri uint32) error {
+
+	res, err := pool.Acquire(ctx)
+	if err != nil {
+		return err
+	}
+	defer res.Release()
+	return res.Value().Bury(id, pri)
+}
+
+/*
 func Get() (*puddle.Resource[*beanstalk.Conn], error) {
 
-	return pool.Acquire(context.Background())
+	return pool.Acquire(ctx)
 }
+*/
 
 func Close() {
 	pool.Close()

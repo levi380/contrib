2dbd52acf438763ebcc57e33cb5bfa3db210d8da
Levi <levi@gmail.com>
2025-11-20 18:24:09 +0800

    id优化

--- helper/sonyflake.go
+++ helper/sonyflake.go
@@ -1,14 +1,50 @@
 package helper
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
+
 	"github.com/yitter/idgenerator-go/idgen"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/concurrency"
 )
 
+func Initial(endpoints []string) {
 
+	var (
+		mctx  = context.Background()
+		mutex *concurrency.Mutex
+	)
+	// 根据etcd 生成work id
+	client, err := clientv3.New(clientv3.Config{Endpoints: endpoints, DialTimeout: time.Second * 3})
+	if err != nil {
+		log.Fatalf("客户端初始化失败:%v\n", err)
+	}
 
-func Initial(workid uint16) {
-	var options = idgen.NewIdGeneratorOptions(workid)
+	sess, err := concurrency.NewSession(client, concurrency.WithTTL(30))
+	if err != nil {
+		log.Fatalf("Session初始化失败:%v\n", err)
+		return
+	}
+
+	workId := 0
+	for i := 1; i < 4095; i++ {
+		// 获取指定前缀的锁对象
+		key := fmt.Sprintf("my-lock/%d", i)
+		mutex = concurrency.NewMutex(sess, key)
+
+		// 加锁默认等待3s
+		err = mutex.TryLock(mctx)
+		if err == nil {
+			workId = i
+			fmt.Printf("尝试获取work id:%s\n", key)
+			break
+		}
+	}
+
+	var options = idgen.NewIdGeneratorOptions(uint16(workId))
 	options.WorkerIdBitLength = 12
 	idgen.SetIdGenerator(options)
 }

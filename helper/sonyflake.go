9d56ba2cf83d85ace573a8474cb3bbacd37b91f8
levi380 <levi@filbetph.com>
2025-12-12 14:49:19 +0800

    Update sonyflake.go

--- helper/sonyflake.go
+++ helper/sonyflake.go
@@ -1,56 +1,18 @@
 package helper
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/sony/sonyflake/v2"
-	clientv3 "go.etcd.io/etcd/client/v3"
-	"go.etcd.io/etcd/client/v3/concurrency"
 )
 
 var sf *sonyflake.Sonyflake
 
-func Initial(endpoints []string) {
-
-	var (
-		mctx  = context.Background()
-		mutex *concurrency.Mutex
-	)
-	// 根据etcd 生成work id
-	client, err := clientv3.New(clientv3.Config{Endpoints: endpoints, DialTimeout: time.Second * 3})
-	if err != nil {
-		log.Fatalf("客户端初始化失败:%v\n", err)
-	}
-
-	sess, err := concurrency.NewSession(client, concurrency.WithTTL(30))
-	if err != nil {
-		log.Fatalf("Session初始化失败:%v\n", err)
-		return
-	}
-
-	workId := 0
-	for i := 1; i < 4095; i++ {
-		// 获取指定前缀的锁对象
-		key := fmt.Sprintf("my-lock/%d", i)
-		mutex = concurrency.NewMutex(sess, key)
-
-		// 加锁默认等待3s
-		err = mutex.TryLock(mctx)
-		if err == nil {
-			workId = i
-			fmt.Printf("尝试获取work id:%s\n", key)
-			break
-		}
-	}
-
-
+func init() {
+	var err error
 	st := sonyflake.Settings{}
-	st.MachineID = func() (int, error) {
-            return workId, nil 
-    }
+
 	sf, err = sonyflake.New(st) // 使用默认设置
 	if err != nil {
 		log.Fatalf("failed to create sonyflake: %v", err)
